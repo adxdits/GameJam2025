36d20bf31b55deeae0c7b0bce542cff14e49ba79
KlayyMore <kypoTV@gmail.com>
2025-09-17 18:27:07 +0200

    fix : cast qte

--- game/casts/cast.py
+++ game/casts/cast.py
@@ -17,9 +17,9 @@ class Cast:
         self.current_combo = []          # <-- vider l'ancienne combo
         self.index_current_combo = 0
         self.current_combo.extend(self.combinations["TYPES"][words[0]])
-        if lvl >= 2: 
+        if lvl >= 1: 
             self.current_combo.extend(self.combinations["QUALIFICATIFS"][words[1]])
-        if lvl >= 3: 
+        if lvl >= 2: 
             self.current_combo.extend(self.combinations["CIBLES"][words[2]])
         
     def check_qte(self, key):
